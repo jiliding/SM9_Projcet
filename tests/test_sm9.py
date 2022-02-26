0ef4b46ec50a200f711dbb8f8be52afb82d63add
nbdl <nbdingliang@qq.com>
2022-02-27 00:47:51 +0800

    注释：SM9秘钥封装与签名

--- tests/test_sm9.py
+++ tests/test_sm9.py
@@ -12,13 +12,18 @@ if __name__ == '__main__':
     # (mpk, msk) ← Setup(λ).已知系统安全参数λ,系统建立算法Setup以λ为输入,输出系统主
     # 公钥mpk和主私钥msk,其中mpk是公开的,msk由KGC秘密保存.此算法由KGC执行
 
-    Da = sm9.private_key_extract ('sign', master_public, master_secret, idA)#skID ← KeyGen(mpk, msk,ID).已知标识ID,用户私钥生成算法KeyGen以系统主公私钥
-    #   对 (mpk, msk) 和 ID 为输入, 输出用户 ID 的私钥 skID. 此算法由 KGC 执行.
+    Da = sm9.private_key_extract ('sign', master_public, master_secret, idA)
+    # skID ← KeyGen(mpk, msk,ID).已知标识ID,用户私钥
+    # 生成算法KeyGen以系统主公私钥对 (mpk, msk) 和 ID 为输入, 输出用户 ID 的私钥 skID. 此算法由 KGC 执行.
 
     message = 'abc'
     signature = sm9.sign (master_public, Da, message)
+    # σ ← Sign(mpk, M, skID).已知消息M, 签名算法Sign以系统主公钥mpk、M
+    # 和签名者的私钥skID为输入, 输出M的签名σ.此算法由签名者执行.
 
     assert (sm9.verify (master_public, idA, message, signature))
+    # 1/0 ← Verify(mpk, M, σ, ID).验证算法Verify以系统主公钥mpk、签名消息M及其签名σ和签名者的标识ID
+    # 为输入, 输出“1”或者“0”.“1”表示签名有效,“0”表示签名无效.此算法由验证者执行.
 
     print ("\t\t\t success")
 
@@ -26,7 +31,7 @@ if __name__ == '__main__':
     
     master_public, master_secret = sm9.setup ('keyagreement')
 
-    Da = sm9.private_key_extract ('keyagreement', master_public, master_secret, idA)
+    Da = sm9.private_key_extract ('keyagreement', master_ public, master_secret, idA)
     Db = sm9.private_key_extract ('keyagreement', master_public, master_secret, idB)
 
     xa, Ra = sm9.generate_ephemeral (master_public, idB)
